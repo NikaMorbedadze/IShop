f6c7582d98c37d87dca3fef6b85f232fc1c9b9ea
n.morbedadze <n.morbedadze@aldagi.ge>
2022-06-15 15:18:20 +0400

    Added Project

---
+++ ISop.Domain/Entities/Person.cs
@@ -0,0 +1,13 @@
+﻿using ISop.Domain.Common;
+using ISop.Domain.Enums;
+
+namespace ISop.Domain.Entities;
+
+public class Person : BaseDomainEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public GenderEnum Gender { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string PhoneNumber { get; set; }
+}
---
+++ ISop.Domain/Enums/GenderEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace ISop.Domain.Enums;
+
+public enum GenderEnum
+{
+    Male=1,
+    Female=2
+}
